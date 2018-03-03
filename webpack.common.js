1e2ee3bf3a23acc00d2a3ae9634ca5effdcee469
Dyesse YUMBA <yumba.nduba@gmail.com>
2018-03-03 22:26:43 +0100

    Enabled hot reloading.

--- webpack.common.js
+++ webpack.common.js
@@ -8,36 +8,34 @@ module.exports = {
   entry: [path.resolve(__dirname, "src/Index.tsx")],
 
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-    modules: [path.resolve(__dirname), "node_modules", "src"]
+    extensions: [
+      ".ts", ".tsx", ".js", ".jsx"
+    ],
+    modules: [
+      path.resolve(__dirname),
+      "node_modules",
+      "src"
+    ]
   },
   target: "web",
-  plugins: [
-    // Generate an external css file with a hash in the filename
-    new SimpleProgressPlugin()
-  ],
+  plugins: [// Generate an external css file with a hash in the filename
+    new SimpleProgressPlugin()],
 
   module: {
     rules: [
       {
         test: /\.eot(\?.*)?$/,
         loader: "file-loader?name=fonts/.[ext]"
-      },
-      {
+      }, {
         test: /\.(woff|woff2)(\?.*)?$/,
         loader: "file-loader?name=fonts/.[ext]"
-      },
-      {
+      }, {
         test: /\.ttf(\?.*)?$/,
-        loader:
-          "url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/.[ext]"
-      },
-      {
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/.[ext]"
+      }, {
         test: /\.svg(\?.*)?$/,
-        loader:
-          "url-loader?limit=10000&mimetype=image/svg+xml&name=fonts/.[ext]"
-      },
-      {
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml&name=fonts/.[ext]"
+      }, {
         test: /\.(jpe?g|png|gif|ico)$/i,
         include: path.resolve("./src"),
         exclude: /node_modules/,
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,10 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const SimpleProgressPlugin = require("webpack-simple-progress-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+
+const port = 3003;
 
 module.exports = merge(common, {
   devtool: "source-map",
@@ -21,8 +22,7 @@ module.exports = merge(common, {
 
   plugins: [
     // Create HTML file that includes reference to bundled JS.
-    new HtmlWebpackPlugin({ template: "src/index.html", inject: true }),
-    new CheckerPlugin(),
+    new HtmlWebpackPlugin({template: "src/index.html", inject: true}),
 
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
@@ -37,34 +37,48 @@ module.exports = merge(common, {
       {
         test: /\.(t|j)sx?$/,
         exclude: /node_modules/,
-        loaders: ["awesome-typescript-loader"]
-      },
-      {
+        use: ['babel-loader', 'ts-loader'],
+      }, {
         enforce: "pre",
         test: /\.js$/,
         loader: "source-map-loader"
-      },
-      {
+      }, {
         test: /\.css$/,
         loaders: ["style-loader", "css-loader"]
-      },
-      {
+      }, {
         test: /\.scss$/,
         use: [
           {
             loader: "style-loader"
-          },
-          {
+          }, {
             loader: "css-loader",
             options: {
               sourceMap: true
             }
-          },
-          {
+          }, {
             loader: "fast-sass-loader"
           }
         ]
       }
     ]
+  },
+  devServer: {
+    contentBase: "/",
+    compress: true,
+    port,
+    hot: true,
+    open: true,
+
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /^\/$/,
+          to: '/index.html'
+        }, {
+          from: /^\/silent_renew.html/,
+          to: 'src/security/silentRenew/silent_renew.html'
+        }
+      ]
+    }
   }
 });
